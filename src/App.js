7fe03c52343e931d85b0ab9bf7bb94cb028f1c34
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-05-30 17:35:54 +0100

    initialize task to an empty array

--- src/App.js
+++ src/App.js
@@ -2,18 +2,7 @@ import React from "react";
 import TodoForm from "./components/TodoComponents/TodoForm";
 import TodoList from "./components/TodoComponents/TodoList";
 
-const tasksArr = [
-  {
-    task: "Organize Garage",
-    id: 1528817077286,
-    completed: false
-  },
-  {
-    task: "Bake Cookies",
-    id: 1528817084358,
-    completed: false
-  }
-];
+const tasksArr = [];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
